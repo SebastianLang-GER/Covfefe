4a260f688ea08a94ad8376d42ce216c842396306
Sebastian Lang <software@sebastianlang.net>
2018-06-12 00:50:17 +0200

    Überarbeitung JavaDoc, Änderung der Reihenfolge: set, get

--- Covfefe/src/covfefe/InputParameter.java
+++ Covfefe/src/covfefe/InputParameter.java
@@ -1,196 +1,214 @@
 package covfefe;
 
+/**
+ * Klasse, zur Übergabe von zur Verfügung stehenden Eingabeparametern
+ * @author Shayan Jani
+ * @version 12.06.2018
+ */
 public class InputParameter {
-	private boolean totalVoltage;
-	private boolean voltage1;
-	private boolean voltage2;
-	private boolean totalResistor;
-	private boolean resistor1;
-	private boolean resistor2;
-	private boolean minResistor1;
-	private boolean minResistor2;
-	private boolean factorResistor1toResistor2;
-	private boolean factorResistor2toResistor1;
-	private boolean ratioResistor1toResistor2;
-	private boolean ratioResistor2toResistor1;
+	
+	private boolean totalVoltage = false;
+	private boolean voltage1 = false;
+	private boolean voltage2 = false;
+	private boolean totalResistor = false;
+	private boolean resistor1 = false;
+	private boolean resistor2 = false;
+	private boolean minResistor1 = false;
+	private boolean minResistor2 = false;
+	private boolean factorResistor1toResistor2 = false;
+	private boolean factorResistor2toResistor1 = false;
+	private boolean ratioResistor1toResistor2 = false;
+	private boolean ratioResistor2toResistor1 = false;
 	
 	/**
-	 * Rückgabe des Werts von totalVoltage 
-	 * @return the totalVoltage
+	 * Festlegen oder ändern des Attributs totalVoltage
+	 * @param value Attribut totalVoltage
+	 */
+	public void setTotalVoltage(boolean value) {
+		totalVoltage = value;
+	}
+	
+	/**
+	 * Rückgabe des Attributs totalVoltage
+	 * @return Attribut totalVoltage
 	 */
 	public boolean getTotalVoltage() {
 		return totalVoltage;
 	}
 	
 	/**
-	 * Festlegen des Werts von totalVoltage 
-	 * @param totalVoltage the totalVoltage to set
+	 * Festlegen oder ändern des Attributs voltage1 
+	 * @param value Attribut voltage1
 	 */
-	public void setTotalVoltage(boolean totalVoltage) {
-		this.totalVoltage = totalVoltage;
+	public void setVoltage1(boolean value) {
+		voltage1 = value;
 	}
 	
 	/**
-	 * Rückgabe des Werts von voltage1
-	 * @return the voltage1
+	 * Rückgabe des Attributs voltage1
+	 * @return Attribut voltage1
 	 */
 	public boolean getVoltage1() {
 		return voltage1;
 	}
 	
 	/**
-	 * Festlegen des Werts von voltage1 
-	 * @param voltage1 the voltage1 to set
+	 * Festlegen oder ändern des Attributs voltage2 
+	 * @param value Attribut voltage2
 	 */
-	public void setVoltage1(boolean voltage1) {
-		this.voltage1 = voltage1;
+	public void setVoltage2(boolean value) {
+		voltage2 = value;
 	}
 	
 	/**
-	 * Rückgabe des Werts von voltage2 
-	 * @return the voltage2
+	 * Rückgabe des Attributs voltage2 
+	 * @return Attribut voltage2
 	 */
 	public boolean getVoltage2() {
 		return voltage2;
 	}
-	
+		
 	/**
-	 * Festlegen des Werts von voltage2 
-	 * @param voltage2 the voltage2 to set
+	 * Festlegen oder ändern des Attributs totalResistor 
+	 * @param value Attribut totalResistor
 	 */
-	public void setVoltage2(boolean voltage2) {
-		this.voltage2 = voltage2;
+	public void setTotalResistor(boolean value) {
+		totalResistor = value;
 	}
-	
+
 	/**
-	 * Rückgabe des Werts von totalResistor 
-	 * @return the totalResistor
+	 * Rückgabe des Attributs totalResistor 
+	 * @return Attribut totalResistor
 	 */
 	public boolean getTotalResistor() {
 		return totalResistor;
 	}
-	
+		
 	/**
-	 * Festlegen des Werts von totalResistor 
-	 * @param totalResistor the totalResistor to set
+	 * Festlegen oder ändern des Attributs resistor1 
+	 * @param value Attribut resistor1
 	 */
-	public void setTotalResistor(boolean totalResistor) {
-		this.totalResistor = totalResistor;
+	public void setResistor1(boolean value) {
+		resistor1 = value;
 	}
-	
+
 	/**
-	 * Rückgabe des Werts von resistor1 
-	 * @return the resistor1
+	 * Rückgabe des Attributs resistor1 
+	 * @return Attribut resistor1
 	 */
 	public boolean getResistor1() {
 		return resistor1;
 	}
 	
 	/**
-	 * Festlegen des Werts von resistor1 
-	 * @param resistor1 the resistor1 to set
+	 * Festlegen oder ändern des Attributs resistor2 
+	 * @param value Attribut resistor2
 	 */
-	public void setResistor1(boolean resistor1) {
-		this.resistor1 = resistor1;
+	public void setResistor2(boolean value) {
+		resistor2 = value;
 	}
+	
 	/**
-	 * Rückgabe des Werts von resistor2 
-	 * @return the resistor2
+	 * Rückgabe des Attributs resistor2 
+	 * @return Attribut resistor2
 	 */
 	public boolean getResistor2() {
 		return resistor2;
 	}
+	
 	/**
-	 * Festlegen des Werts von resistor2 
-	 * @param resistor2 the resistor2 to set
+	 * Festlegen oder ändern des Attributs minResistor1
+	 * @param value Attribut minResistor1
 	 */
-	public void setResistor2(boolean resistor2) {
-		this.resistor2 = resistor2;
+	public void setMinResistor1(boolean value) {
+		minResistor1 = value;
 	}
+	
 	/**
-	 * Rückgabe des Werts von minResistor1 
-	 * @return the minResistor1
+	 * Rückgabe des Attributs minResistor1 
+	 * @return Attribut minResistor1
 	 */
 	public boolean getMinResistor1() {
 		return minResistor1;
 	}
+
 	/**
-	 * 	Festlegen des Werts von minResistor1
-	 * @param minResistor1 the minResistor1 to set
+	 * Festlegen oder ändern des Attributs minResistor2 
+	 * @param value Attribut minResistor2
 	 */
-	public void setMinResistor1(boolean minResistor1) {
-		this.minResistor1 = minResistor1;
+	public void setMinResistor2(boolean value) {
+		minResistor2 = value;
 	}
+	
 	/**
-	 * Rückgabe des Werts von minResistor2 
-	 * @return the minResistor2
+	 * Rückgabe des Attributs minResistor2 
+	 * @return Attribut minResistor2
 	 */
 	public boolean getMinResistor2() {
 		return minResistor2;
 	}
+	
 	/**
-	 * Festlegen des Werts von minResistor2 
-	 * @param minResistor2 the minResistor2 to set
+	 * Festlegen oder ändern des Attributs factorResistor1toResistor2 
+	 * @param value Attribut factorResistor1toResistor2
 	 */
-	public void setMinResistor2(boolean minResistor2) {
-		this.minResistor2 = minResistor2;
+	public void setFactorResistor1toResistor2(boolean value) {
+		factorResistor1toResistor2 = value;
 	}
+	
 	/**
-	 *  Rückgabe des Werts von factorResistor1toResistor2 
-	 * @return the factorResistor1toResistor2
+	 *  Rückgabe des Attributs factorResistor1toResistor2 
+	 * @return Attribut factorResistor1toResistor2
 	 */
 	public boolean getFactorResistor1toResistor2() {
 		return factorResistor1toResistor2;
 	}
+	
 	/**
-	 * Festlegen des Werts von factorResistor1toResistor2 
-	 * @param factorResistor1toResistor2 the factorResistor1toResistor2 to set
+	 * Festlegen oder ändern des Attributs factorResistor2toResistor1 
+	 * @param value Attribut factorResistor2toResistor1
 	 */
-	public void setFactorResistor1toResistor2(boolean factorResistor1toResistor2) {
-		this.factorResistor1toResistor2 = factorResistor1toResistor2;
+	public void setFactorResistor2toResistor1(boolean value) {
+		factorResistor2toResistor1 = value;
 	}
+	
 	/**
-	 * Rückgabe des Werts von factorResistor2toResistor1 
-	 * @return the factorResistor2toResistor1
+	 * Rückgabe des Attributs factorResistor2toResistor1 
+	 * @return Attribut factorResistor2toResistor1
 	 */
 	public boolean getFactorResistor2toResistor1() {
 		return factorResistor2toResistor1;
 	}
+	
 	/**
-	 * Festlegen des Werts von factorResistor2toResistor1 
-	 * @param factorResistor2toResistor1 the factorResistor2toResistor1 to set
+	 * Festlegen oder ändern des Attributs ratioResistor1toResistor2 
+	 * @param value Attribut ratioResistor1toResistor2
 	 */
-	public void setFactorResistor2toResistor1(boolean factorResistor2toResistor1) {
-		this.factorResistor2toResistor1 = factorResistor2toResistor1;
+	public void setRatioResistor1toResistor2(boolean value) {
+		ratioResistor1toResistor2 = value;
 	}
+	
 	/**
-	 * Rückgabe des Werts von ratioResistor1toResistor2 
-	 * @return the ratioResistor1toResistor2
+	 * Rückgabe des Attributs ratioResistor1toResistor2 
+	 * @return Attribut ratioResistor1toResistor2
 	 */
 	public boolean getRatioResistor1toResistor2() {
 		return ratioResistor1toResistor2;
 	}
+	
 	/**
-	 * Festlegen des Werts von ratioResistor1toResistor2 
-	 * @param ratioResistor1toResistor2 the ratioResistor1toResistor2 to set
+	 * Festlegen oder ändern des Attributs ratioResistor2toResistor1 
+	 * @param value Attribut ratioResistor2toResistor1
 	 */
-	public void setRatioResistor1toResistor2(boolean ratioResistor1toResistor2) {
-		this.ratioResistor1toResistor2 = ratioResistor1toResistor2;
+	public void setRatioResistor2toResistor1(boolean value) {
+		ratioResistor2toResistor1 = value;
 	}
+
 	/**
-	 * Festlegen des Werts von ratioResistor1toResistor2 
-	 * @return the ratioResistor2toResistor1
+	 * Rückgabe des Attributs ratioResistor1toResistor2 
+	 * @return Attribut ratioResistor2toResistor1
 	 */
 	public boolean getRatioResistor2toResistor1() {
 		return ratioResistor2toResistor1;
 	}
-	/**
-	 * Festlegen des Werts von ratioResistor2toResistor1 
-	 * @param ratioResistor2toResistor1 the ratioResistor2toResistor1 to set
-	 */
-	public void setRatioResistor2toResistor1(boolean ratioResistor2toResistor1) {
-		this.ratioResistor2toResistor1 = ratioResistor2toResistor1;
-	}
-	
-
 }
