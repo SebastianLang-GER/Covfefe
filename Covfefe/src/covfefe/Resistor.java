b4096f8d061d736a1a2ad7fc97957f447df7f911
Sebastian Lang <software@sebastianlang.net>
2018-06-12 23:31:49 +0200

    Korrektur und Vervollständigung Resistor(), Überarbeitung JavaDoc

--- Covfefe/src/covfefe/Resistor.java
+++ Covfefe/src/covfefe/Resistor.java
@@ -1,62 +1,85 @@
 package covfefe;
 
+/**
+ * Klasse, die einen ohmschen Widerstand als Bauteil repräsentiert
+ * @author Sebastian Lang, Hussein Kaviani
+ * @version 12.06.2018
+ */
 public class Resistor {
 	
-	private eSeriesValue resistance;
-	private ESeriesTemplate series;
+	private Resistance eSeriesResistance;
+	private ESeriesTemplate eSeries;
 	
 	/**
-	 * Die Klasse Resistor repräsentiert einen ohmschen Widerstand als Bauteil.
-	 * @param value Widerstandswert
+	 * Konstruktor zum Erzeugen von Objekten der Klasse Resistor
+	 * @param resistance Widerstand (physikalische Größe)
+	 * @param eSeries E-Reihe des Widerstands
 	 */
-	public Resistor (double value){
-		resistance = new Resistance();
-		resistance.setValue(value);
-		series = new ESeriesTemplate();
-	}
-	/**
-	 * Methode zur Rückgabe des Widerstandswertes
-	 * @return Widerstandswert
-	 */
-	public double getValue() {
-		return Resistance.getValue();
+	public Resistor(Resistance resistance, ESeriesTemplate eSeries) {
+		eSeriesResistance = new Resistance();
+		setESeries(eSeries);
+		chooseResistor(resistance);
 	}
 	
 	/**
-	 * Festlegen der E-Reihe des Widerstands
-	 * @param value E-Reihe
+	 * Rückgabe des Widerstands (physikalische Größe)
+	 * @return Widerstand (physikalische Größe)
+	 */
+	public Resistance getValue() {
+		return eSeriesResistance;
+	}
+	
+	/**
+	 * Festlegen oder ändern der E-Reihe des Widerstands
+	 * @param value E-Reihe des Widerstands
 	 */
 	public void setESeries(ESeriesTemplate value) {
-		
+		eSeries = value;
 	}
 	
 	/**
-	 * Methode zur Rückgabe der E-Reihe des Widerstands 
-	 * @return E-Reihe
+	 * Rückgabe der E-Reihe des Widerstands
+	 * @return E-Reihe des Widerstands
 	 */
 	public ESeriesTemplate getESeries() {
-		return series;
+		return eSeries;
 	}
 	
 	/**
-	 * Methode zur Festlegung von Einheitvorsätzen
-	 * @param Potenz zur Bestimmung der Einheit
+	 * Auswählen eines Widerstands anhand der E-Reihe
+	 * @param resistance Widerstand (physikalische Größe)
 	 */
-	public void setUnitPrefix(int power) {
+	public void chooseResistor(Resistance resistance) {
+		eSeriesResistance.setPrefix(resistance.getPrefix().getPower()); //Einheitenvorsatz übernehmen
 		
+		//Dezimalfaktor der E-Reihe angleichen
+		int decimalFactor = 0;
+		double value = resistance.getValue();
+		while(Math.abs(value) >= 10) {
+			value = value / 10;
+			if(value > 0) {
+				decimalFactor++; //Dezimalfaktor erhöhen
+			}
+			else {
+				decimalFactor--; //Dezimalfaktor reduzieren
+			}
+		}
+		
+		//Widerstand mit der geringsten Abweichung zur E-Reihe ermitteln
+		double minDifference = Double.MAX_VALUE;
+		for(int i = 0; i < eSeries.getItems(); i++) {
+			double difference = Math.abs(eSeries.getValue(i) * Math.pow(10, decimalFactor) - resistance.getValue()); //Abweichung berechnen
+			if(difference < minDifference) {
+				minDifference = difference;
+				eSeriesResistance.setValue(eSeries.getValue(i) * Math.pow(10, decimalFactor)); //Widerstandswert übernehmen
+			}
+		}
 	}
 	
 	/**
-	 * Methode um Widerstandswert anhand der E-Reihe auszuwählen
-	 * @param value Widerstandswert
-	 */
-	public void chooseResistor(Resistance value) {
-		
-	}
-	/** 
-	 * Methode zur Rückgabe aller Attribute und Eigenschaften als Zeichenkette
+	 * Rückgabe aller Attribute und Eigenschaften der Klasse als Zeichenkette
 	 */
 	public String toString() {
-		return Resistance.toString() + "\n" + series.toString();
+		return "Wert: " + eSeriesResistance.toString() + "\nE-Reihe: " + eSeries.toString();
 	}
 }
