c686abdc1689a87799223706c3d94f4eb570ca7d
Tobias Vöth <tobias.voeth@hs-furtwangen.de>
2018-06-13 17:11:01 +0200

    Erstellen der Klassen Voltage und Resistance

--- Covfefe/src/covfefe/Resistance.java
+++ Covfefe/src/covfefe/Resistance.java
@@ -1,5 +1,17 @@
 package covfefe;
 
-public class Resistance {
+/**
+ * Klasse, welche die Einheit des Widerdstandes festlegt.
+ * @author Dominik Thörmer, Tobias Vöth
+ * @version 13.06.2018
+ */
 
+public abstract class Resistance extends PhysicalQuantity {
+	
+	/**
+	 * Konstruktor zum Festlegen der Einheit "Ohm"
+	 */
+	public Resistance() {
+		super("Ω");
+	}
 }
--- Covfefe/src/covfefe/Voltage.java
+++ Covfefe/src/covfefe/Voltage.java
@@ -1,5 +1,17 @@
 package covfefe;
 
-public abstract class Voltage extends PhysicalQuantity {
+/**
+ * Klasse, welche die Einheit der Spannung festlegt.
+ * @author Dominik Thörmer, Tobias Vöth
+ * @version 13.06.2018
+ */
 
+public abstract class Voltage extends PhysicalQuantity {
+	
+	/**
+	 * Konstruktor zum Festlegen der Einheit "Volt"
+	 */
+	public Voltage() {
+		super("V");
+	}
 }
